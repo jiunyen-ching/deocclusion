2b53c4acc72382f8eba5c641832af34f01e60839
jiunyen.ching <jyching95@hotmail.com>
2022-05-13 02:26:59 +0800

    Delete experiments/pcnet_m directory

--- experiments/pcnet_m/resume.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-work_path=$(dirname $0)
-# ITER=$1
-PRETRAIN=$1
-python -m torch.distributed.launch --nproc_per_node=1 main.py \
-    --config $work_path/config.yaml --launcher pytorch \
-    # --load-iter $ITER --resume
-    --load-pretrain $PRETRAIN --resume
